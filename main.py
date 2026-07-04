fe92ab218bf69af37938090fd0f273ae9d2fed3b
gaytaras-dot <gaytaras@gmail.com>
2026-07-04 14:44:38 +0300

    Первый коммит. Домашнее задание по изолир/среде

---
+++ main.py
@@ -0,0 +1,46 @@
+# main
+# Домашнее задание от 01.07.2026
+# Демонстрация некоторых возможностей Python
+
+def greeting():
+    """
+    greeting(приветствие)
+    Эта функция приветсвует пользователя
+    """
+    print('Привет, мир!')
+    print('Привет лучшим ученикам десятилетия!\nНо это не точно!')
+
+def addition(a, b):
+    """
+    addition(сложение)
+    Эта функция производит сложение двух числел
+    """
+    add = a + b
+    return add
+
+def converting_to_decimal(bynary_str):
+    """
+    converting to decimal(перевод в десятичное) 
+    Эта функция демонстрирует перевод бинарного числа в десятичное
+    """
+    num_list = []
+    for x in bynary_str:
+        num_list.append(int(x))
+    num_list.reverse()
+    decimal = 0
+    for x in num_list:
+        decimal += x * (2 ** num_list.index(x))
+    return decimal
+
+def main():
+    """Основная функция, запускающая программу"""
+    greeting()
+    print(f"Сумма 12 + 7 = {addition(12, 7)}")
+    print(f"Бинарное число 0101011 в десятичном выражении равно {converting_to_decimal('0101011')}")
+
+if __name__ == "__main__":
+    """
+    Функция реализует разделение между модулем (если как модуль - можно 
+    использовать вдругих программах) и скриптом
+    """
+    main()
